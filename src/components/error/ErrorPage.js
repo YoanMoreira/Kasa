3daf7adb56c9e693d4a4af4bb0bf1dfeef327a8e
Moreira <yoan.moreira.ud@gmail.com>
2023-11-23 10:00:05 +0100

    errorcomment

--- src/components/error/ErrorPage.js
+++ src/components/error/ErrorPage.js
@@ -1,12 +1,20 @@
 import { NavLink } from "react-router-dom";
-export default function NoPage() {
+
+// Composant NoPage : Affiche une page d'erreur 404.
+function NoPage() {
   return (
     <>
+      {/* Conteneur de l'erreur */}
       <div className="errorContainer">
+        {/* Numéro de l'erreur */}
         <p className="errorContainer__Number">404</p>
+
+        {/* Texte de l'erreur */}
         <p className="errorContainer__Text">
           Oups! La page que vous demandez n'existe pas.
         </p>
+
+        {/* Lien pour retourner à la page d'accueil */}
         <NavLink to="/" className="errorContainer__Link">
           Retourner sur la page d'accueil
         </NavLink>
@@ -14,3 +22,5 @@ export default function NoPage() {
     </>
   );
 }
+
+export default NoPage;
