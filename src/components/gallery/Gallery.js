d21658e8ab7546d5a94ed296855bf6a7d7f2ad66
Moreira <yoan.moreira.ud@gmail.com>
2023-11-23 10:01:31 +0100

    galcomment

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import logements from "../../datas/logement.json";
 import Card from "../cards/Card";
 
+// Composant Gallery : Affiche une galerie de logements.
 const Gallery = () => {
   return (
     <div className="home_gallery">
+      {/* Boucle sur chaque logement dans le fichier JSON */}
       {logements.map((logement) => {
         return (
           <article key={logement.id}>
+            {/* Lien vers la page détaillée du logement */}
             <Link to={`/logement/${logement.id}`}>
+              {/* Utilisation du composant Card pour afficher les détails du logement */}
               <Card image={logement.cover} title={logement.title} />
             </Link>
           </article>
